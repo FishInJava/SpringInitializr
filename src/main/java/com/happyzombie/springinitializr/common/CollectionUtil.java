e8e9a0954fb485fd1d7dca64b832b03933fbe547
hbz <294975286@qq.com>
2022-03-09 13:24:57 +0800

    -add utils and guava

---
+++ src/main/java/com/happyzombie/springinitializr/common/CollectionUtil.java
@@ -0,0 +1,31 @@
+package com.happyzombie.springinitializr.common;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 套壳的目的是为了方便整体替换
+ */
+public class CollectionUtil {
+
+    /**
+     * 将一个list均分成n个list
+     *
+     * 每份n个
+     */
+    public static <T> List<List<T>> partition(List<T> source, int n) {
+        return Lists.partition(source, n);
+    }
+
+    public static boolean isEmpty(final Collection<?> coll) {
+        return CollectionUtils.isEmpty(coll);
+    }
+
+    public static boolean isNotEmpty(final Collection<?> coll) {
+        return CollectionUtils.isEmpty(coll);
+    }
+
+}
