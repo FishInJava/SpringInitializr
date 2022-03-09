5ba8de14131e47156ca168451e01d50e2b9c7bf3
hbz <294975286@qq.com>
2022-03-09 19:38:14 +0800

    -add some Servcie

--- src/main/java/com/happyzombie/springinitializr/SpringInitializrApplication.java
+++ src/main/java/com/happyzombie/springinitializr/SpringInitializrApplication.java
@@ -1,11 +1,13 @@
 package com.happyzombie.springinitializr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@MapperScan({"com.happyzombie.springinitializr.dao"})
 public class SpringInitializrApplication {
 
     public static void main(String[] args) {
