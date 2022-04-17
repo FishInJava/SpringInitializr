3161c1380edbc0c780f68bb1c2e7324fb07696c9
hbz <294975286@qq.com>
2022-04-17 11:34:37 +0800

    -硬编码：过滤掉delete_request和confirm方法

--- src/main/java/com/happyzombie/springinitializr/bean/request/user/GetUserTransactionsRequest.java
+++ src/main/java/com/happyzombie/springinitializr/bean/request/user/GetUserTransactionsRequest.java
@@ -3,10 +3,16 @@ package com.happyzombie.springinitializr.bean.request.user;
 import com.github.pagehelper.PageInfo;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author admin
  */
 @Data
 public class GetUserTransactionsRequest extends PageInfo {
     private String userAccountId;
+    /**
+     * 过滤掉的方法名称
+     */
+    private List<String> filterMethodName;
 }
